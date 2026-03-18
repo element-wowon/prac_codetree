91648de84f944eae7374967b53a337de496742be
element-wowon <lisa2322@naver.com>
2026-03-19 08:48:24 +0900

    Title: 문자 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260319/문자 출력하기/print-text.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char ch;
+    scanf("%c",&ch);
+
+    for(int i=0; i<8; i++)
+        printf("%c",ch);
+    return 0;
+}
