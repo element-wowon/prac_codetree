7930bf7f46af4082e1bb11ab1d7401f5f3b8e320
element-wowon <lisa2322@naver.com>
2026-03-18 11:03:53 +0900

    Title: 비교 연산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260318/비교 연산/comparison-operator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int A,B;
+    scanf("%d %d",&A,&B);
+    printf("%d\n",A>=B);
+    printf("%d\n",A>B);
+    printf("%d\n",B>=A);
+    printf("%d\n",B>A);
+    printf("%d\n",A==B);
+    printf("%d\n",A!=B);
+    return 0;
+}
