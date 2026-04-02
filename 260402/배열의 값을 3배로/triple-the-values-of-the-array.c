afeba4f14c28947e5da8163e5c5590c6175ab348
element-wowon <lisa2322@naver.com>
2026-04-02 18:07:09 +0900

    Title: 배열의 값을 3배로, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/배열의 값을 3배로/triple-the-values-of-the-array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int arr[3][3];
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            scanf("%d",&arr[i][j]);
+            printf("%d ",arr[i][j]*3);
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
